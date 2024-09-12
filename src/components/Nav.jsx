04d08fda1758221f08e033d55335b94d5263f262
AkashJana18 <akashjana663@gmail.com>
2024-09-12 23:48:53 +0530

    Navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container ">
+        <a href="/">
+          <img src={headerLogo} width={130} height={29} alt="logo" />
+        </a>
+
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block px-5">
+          <img src={hamburger} alt="menu-icon" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
