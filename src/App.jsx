1d443657a13a8ef7ad20d3913649e83a775104ff
AkashJana18 <akashjana663@gmail.com>
2024-09-12 15:27:37 +0530

    File structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
+import {
+  CustomerReviews,
+  Nav,
+  Footer,
+  Hero,
+  PopularProduct,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <main>
+      <section className="padding">
+        <Nav />
+      </section>
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProduct /> 
+      </section>
+      <section className="padding">
+        <SuperQuality /> 
+      </section>
+      <section className="padding-y py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer /> 
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews /> 
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x padding-t bg-black pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
 }
---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import Nav from "./Nav";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  Nav,
+  CustomerReviews,
+  Footer,
+  PopularProduct,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
