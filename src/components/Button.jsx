858d7a5223fe77fe4d2def61d232fa686cec94dd
AkashJana18 <akashjana663@gmail.com>
2024-09-14 19:03:14 +0530

    button update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,28 @@
-
-
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className="flex justify-center items-center px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
       {label}
-      {iconUrl && (
-        <img src={iconUrl} alt="button" className="ml-2 rounded-full w-5 h-5" />
+
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt='arrow right icon'
+          className='ml-2 rounded-full bg-white w-5 h-5'
+        />
       )}
     </button>
   );
