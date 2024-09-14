d9a6efc5c17a7a9b71ba2a976fe535e4b178d756
AkashJana18 <akashjana663@gmail.com>
2024-09-14 18:28:55 +0530

    "Updated PopularProduct component with new UI and functionality"

--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { products } from "../constants/index";
+import PopularProductCard from "../components/PopularProductCard";
 
 const PopularProduct = () => {
   return (
-    <div>PopularProduct</div>
-  )
-}
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          Out <span className="text-coral-red">Popular</span> Products
+        </h2>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
+          Experience top-notch quality and style with our sought-afer
+          selections. Discover a world of comfort, design and value
+        </p>
+      </div>
+      <div className="grid mt-16 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default PopularProduct
+export default PopularProduct;
