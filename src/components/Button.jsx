22f6641481fe9e12c1d9fa290d35aaf7d057f2e5
AkashJana18 <akashjana663@gmail.com>
2024-09-14 18:59:07 +0530

    bug fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
 
-const Button = ({label, iconUrl }) => {
+
+const Button = ({ label, iconUrl }) => {
   return (
     <button className="flex justify-center items-center px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white">
-        {label}
-        <img src={iconUrl} alt="button"
-        className='ml-2 rounded-full w-5 h-5' />
+      {label}
+      {iconUrl && (
+        <img src={iconUrl} alt="button" className="ml-2 rounded-full w-5 h-5" />
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
