015c162f3d3a365b479b283d957633a840bb128d
AkashJana18 <akashjana663@gmail.com>
2024-09-14 17:42:04 +0530

    button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = ({label, iconUrl }) => {
+  return (
+    <button className="flex justify-center items-center px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white">
+        {label}
+        <img src={iconUrl} alt="button"
+        className='ml-2 rounded-full w-5 h-5' />
+    </button>
+  )
+}
+
+export default Button
